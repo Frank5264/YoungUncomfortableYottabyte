403f48d2cfc248f473a627f752dfa1352605a6c9
Frank5264 <73259057+Frank5264@users.noreply.github.com>
2024-03-02 08:55:31 +0000

    New

--- main.js
+++ main.js
@@ -26,6 +26,10 @@ const {proto} = (await import('@whiskeysockets/baileys')).default;
 const {DisconnectReason, useMultiFileAuthState, MessageRetryMap, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser, PHONENUMBER_MCC} = await import('@whiskeysockets/baileys');
 import readline from 'readline';
 import NodeCache from 'node-cache';
+import moment from 'moment-timezone';
+import broadcast from './lib/broadcast.js';
+import { menu_number } from './lib/menu_number.js';
+import { db_menu } from './lib/db_menu.js';
 const {CONNECTING} = ws;
 const {chain} = lodash;
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
@@ -536,7 +540,70 @@ setInterval(async () => {
 
 
 
+broadcast(client, MessageType, Mimetype);    
 
+
+async function start() {
+
+    
+
+    client.on('chat-update', async (msg) => {
+        
+        if (msg.messages && msg.count && msg.hasNewMessage && client.contacts[msg.messages.all()[0].key.remoteJid] != undefined) {
+
+            const Menufrom = getMenu({ from: msg.messages.all()[0].key.remoteJid });
+                
+            menu_number[Menufrom].menu_name.exec({
+
+                messages: msg.messages.all()[0].message,
+                download_msg: msg.messages.all()[0],
+                Mimetype: Mimetype ,
+                from: msg.messages.all()[0].key.remoteJid,
+                body: msg.messages.all()[0].message.conversation,
+                MessageType: MessageType,
+                isGroup: msg.messages.all()[0].key.remoteJid.endsWith('@g.us'),
+                pushname: client.contacts[msg.messages.all()[0].key.remoteJid].name || client.contacts[msg.messages.all()[0].key.remoteJid].notify,
+                client: client,            
+
+            });   
+
+        }
+
+    });
+
+    client.on('group-participants-update', async (msg) => {
+
+        const group_user = fs.readJsonSync('./db/group_user.json');
+
+        if (msg.jid && msg.action === 'remove' && msg.participants.includes(client.user.jid)){
+
+            let del = group_user.indexOf(msg.jid);
+            group_user.splice(del, 1)
+            fs.writeJsonSync('./db/group_user.json', group_user)
+            
+        }  
+
+    });
+    
+}
+
+function getMenu({ from }) {
+    
+    if (db_menu[from]){
+
+        return db_menu[from].menu_name;
+    }
+
+    else {
+
+        db_menu[from] = { menu_name: 0 };
+        return db_menu[from].menu_name;
+
+    }
+
+}
+
+start()
 
 
 
